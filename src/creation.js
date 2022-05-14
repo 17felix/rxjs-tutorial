5bae36501bd0f7eaae36f44e5c326da3a4c0521b
17felix <19fmysko@gmail.com>
2022-05-14 16:49:14 +0300

    with fromEvent: drawing

--- src/creation.js
+++ src/creation.js
@@ -36,3 +36,23 @@ streamObservable$.subscribe({
         console.log('II: Complete');
     }
 })
+
+
+// FromEvent
+fromEvent(document.querySelector('canvas'), 'mousemove')
+.pipe(
+    map(e => ({
+        x: e.offsetX,
+        y: e.offsetY,
+        ctx: e.target.getContext('2d')
+    }))
+)
+.subscribe(pos => {
+    pos.ctx.fillRect(pos.x, pos.y, 2, 2)
+})
+
+const clear$ = fromEvent(document.getElementById('clear'), 'click')
+clear$.subscribe(() => {
+    const canvas = document.querySelector('canvas')
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+})
