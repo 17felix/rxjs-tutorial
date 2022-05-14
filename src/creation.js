43ebaec8bc078a9f87a8b47ebdd2aa37e048b9d5
17felix <19fmysko@gmail.com>
2022-05-14 16:48:40 +0300

    with Observable

--- src/creation.js
+++ src/creation.js
@@ -11,3 +11,28 @@ const arr$ = from([1, 2, 3, 4]).pipe(
 )
 arr$.subscribe(val => console.log(val))
 
+const streamObservable$ = new Observable(observer => {
+    observer.next('First value')
+    setTimeout(() => observer.next('After 1000ms'), 1000)
+    setTimeout(() => observer.complete, 2000)           // comment complete() to move forward
+    setTimeout(() => observer.error('Something went wrong'), 3000)  // comment erroe() to move forward
+    setTimeout(() => observer.next('After 3000ms'), 4000)
+})
+// I: 3 callback, one by one
+streamObservable$.subscribe(
+    (val) => console.log('Val:', val),
+    (err) => console.log(err),
+    () => console.log('I: Complete'),
+)
+// II: using object keys
+streamObservable$.subscribe({
+    next(val) {
+        console.log(val);
+    }, 
+    error(err) {
+        console.log(err);
+    }, 
+    complete() {
+        console.log('II: Complete');
+    }
+})
