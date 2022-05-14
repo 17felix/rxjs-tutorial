0417b458fe90de47738890f6cd1d6ee82dde72d9
17felix <19fmysko@gmail.com>
2022-05-14 16:46:49 +0300

    with from

--- src/creation.js
+++ src/creation.js
@@ -5,3 +5,9 @@ const stream$ = of('Hello', 'World')
 stream$.subscribe(val => {
     console.log('Value: ', val);
 })
+
+const arr$ = from([1, 2, 3, 4]).pipe(
+    scan((acc, v) => acc.concat(v), [])
+)
+arr$.subscribe(val => console.log(val))
+
